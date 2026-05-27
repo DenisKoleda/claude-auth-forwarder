47330a9d9c436972d689bb00248bfee09d13e891
DenisKoleda <deniskoleda6@gmail.com>
2026-05-27 12:51:28 +0400

    Suppress low-level HTTP request logs

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ logging.basicConfig(
     level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", datefmt="%H:%M:%S"
 )
 logger = logging.getLogger(__name__)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 
 def validate_config() -> None:
